84530f0df7de5865b6c52f9f7d4f81e493e2b307
Wilmer Guedez <w.guedex@gmail.com>
2024-06-29 21:29:58 -0500

    agregar health-check

---
+++ src/health-check/health-check.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('/')
+export class HealthCheckController {
+
+    @Get()
+    healthCheck() {
+        return 'Client Gateway is up and running!!!';
+    }
+    
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 import { RpcCustomExceptionFilter } from './common';
 
@@ -18,7 +18,13 @@ async function bootstrap() {
 
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api',{
+    exclude:[{
+      path: '', 
+      method: RequestMethod.GET
+      }]
+
+  });
 
   await app.listen(envs.port);
 
